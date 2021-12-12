2b76f3a002bc93ae4a85b7c621eb8a6515c22829
Mauricio de Assis <mauriciox8mm@hotmail.com>
2021-12-12 12:00:10 -0300

    ajustes

--- conversor_moeda/lib/main.dart
+++ conversor_moeda/lib/main.dart
@@ -1,16 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
-
+import 'dart:async';
 const request = "http://api.hgbrasil.com/finance?format=json&key=xxxxxxxx";
 
 void main() async {
 
-  http.Response response = await http.get(Uri.parse(request));
-  print(response.body);
+ // http.Response response = await http.get(Uri.parse(request));
+  //jsonDecode(response.body)["results"]["currencies"]["USD"]
 
+  //print( jsonDecode(response.body)["results"]["currencies"]["USD"]);
   runApp(MaterialApp(
       home: Container()
   ));
+}
+
+Future<Map> getData() async{
+
+  http.Response response = await http.get(Uri.parse(request));
+  return jsonDecode(response.body);
 }
