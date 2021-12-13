3dc5d772a8edd73179ec95d7bae3cef9e6fb1c69
Mauricio de Assis <mauriciox8mm@hotmail.com>
2021-12-12 21:42:11 -0300

    snackbar

--- lista_app/lib/main.dart
+++ lista_app/lib/main.dart
@@ -18,6 +18,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List _toDoList = [];
   final getTarefa = TextEditingController();
+  late Map<String, dynamic> _lastRemoved;
+  late int _lastRemovedPos;
 
   @override
   void initState() {
@@ -114,6 +116,30 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+
+          _lastRemovedPos = index;
+          _toDoList.remove(index);
+          _saveData();
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]} Removida !"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+
+            ),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
     );
   }
 
